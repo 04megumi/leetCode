fa514cb623e190b12593255fdbb979aee2b698f2
Ubuntu <ubuntu@localhost.localdomain>
2025-02-20 21:45:34 +0800

    twoSum

--- 01_twoSum.cpp
+++ 01_twoSum.cpp
@@ -1,14 +1,16 @@
-class Solution {
-    public:
-        vector<int> twoSum(vector<int>& nums, int target) {
-            unordered_map<int, int> hashMap;
+/*
+    hashMap
+    O(n)
+*/
 
-            for(int i=0;i<nums.size();i++){
-                if(hashMap.count(target - nums[i])){
-                    return {i, hashMap[target-nums[i]]};
-                }
-                hashMap[nums[i]] = i;
-            }
-            return {};
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashMap;
+        for(int i=0;i<nums.size();i++){
+            if(hashMap.count(target - nums[i])) return {i, hashMap[target-nums[i]]};
+            hashMap[nums[i]] = i;
         }
+        return {};
+    }
 };

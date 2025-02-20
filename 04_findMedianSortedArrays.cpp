46372879bb8de0b2f8682e3f1eca331b85a5442a
Ubuntu <ubuntu@localhost.localdomain>
2025-02-20 21:42:10 +0800

    findMedianSortedArrays

--- 04_findMedianSortedArrays.cpp
+++ 04_findMedianSortedArrays.cpp
@@ -1,16 +1,10 @@
 /*
-    题目要求: O(log(m+n))
-
-    初步想法:
-    (1) 二分
-    (2) ==> (m1+m2)=(m+n)/2, max(nums[m1],nums[m2])<min(nums[m1+1],nums[m2+2])
-    (3) 只需找到一种二分方法 快速找到(2)中的解
-
+    time limit: O(log(m+n))
 */
 
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int 
+        int p1 = 0, p2 = nums2.size()-1; //p1 only increases, p2 only decreases
     }
 };

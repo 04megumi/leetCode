5d0d2caf45a5cbf9cc2d84668cb63f12102db93a
seraphimwei <seraphimwei@tencent.com>
2025-02-21 12:35:53 +0800

    超时

--- 04_findMedianSortedArrays.cpp
+++ 04_findMedianSortedArrays.cpp
@@ -3,7 +3,7 @@
     
 ----------------------------------------------------------------
     firstly, there're two pointers: p1, p2.                    
-----------------------------------------------------------------    
+----------------------------------------------------------------
                        a1[1,2,3,4,5]    a2[3,4,5,6]
     consider  (1) odd  meidian is 5th, {p1,p2}={2,1},{3,0}
                        ==> when odd, p1+p2 = (l1+l2+1)/2 - 2;
@@ -23,7 +23,7 @@
                   ==> p1+p2=3
                   actually, {p1,p2}=[2,1]
 ------------------------------------------------------------------
-     conclusion: (1) p1+p2 = (l1+l2)/2 (no matter odd or even)
+     conclusion: (1) p1+p2 = (l1+l2)/2 - 1(no matter odd or even)
                  (2) midian = max(nums1[p1],nums2[p2]) (odd)
                               (nums1[p1]+nums2[p2])/2 (even)
 ------------------------------------------------------------------
@@ -35,6 +35,21 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int p1 = 0, p2 = nums2.size()-1; //p1 only increases, p2 only decreases
+        if(nums1.size()<nums2.size()) return findMedianSortedArrays(nums2, nums1);
+        int p1 = 0, p2 = 0, key = (nums1.size()+nums2.size())/2 - 1;
+        int left = 0, right = nums1.size()-1;
+        if(nums1.size()==1 && !nums2.size()) return nums1[0];
+        while(left<=right){
+            p1 = (right+left) >> 1;
+            p2 = key - p1;
+            if(p1==nums1.size()-1) break;
+            if(p2==nums2.size()-1 && nums2[p2]<nums1[p1+1]) break;
+            if(p2==nums2.size()-1) {left=p1+1; continue;}
+            if(p2<0) {right=p1-1; continue;}
+            if(max(nums1[p1], nums2[p2])<min(nums1[p1+1],nums2[p2+1])) break;
+            if(nums1[p1]>nums2[p2+1]) right=p1-1;
+            if(nums2[p2]>nums1[p1+1]) left=p1+1;
+        }
+        return (nums1.size()+nums2.size())%2 ? max(nums1[p1],nums2[p2]) : (nums1[p1]+nums2[p2])*1.0/2;
     }
 };

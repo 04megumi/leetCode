4abbe82f3fa526f816a19efa841d4f5cbb148fdc
seraphimwei <seraphimwei@tencent.com>
2025-02-28 11:43:24 +0800

    动态规划

--- 05_longestPalindrome.cpp
+++ 05_longestPalindrome.cpp
@@ -1,40 +1,41 @@
 /*
 
-(1) 双指针遍历所有情况 O(n^2)
+(1) 双指针遍历所有情况 O(n^3)
 
 (2) 第一次遍历 ==> map<char, vector<int>>
     后续只要再一次遍历, 每次只遍历首字母索引
-    最好O(n), 最差O(n^2)
+    最好O(n^2), 最差O(n^3)
+
+(3) 动态规划 ==> 维护 dp[n][n], begin, maxLen
+    
+    s[i] != s[j] ==> dp[i][j] = false
+    s[i] == s[j] ==> dp[i][j] = j-i<3 ? true : dp[i+1][j-1]
 
 */
 class Solution {
 public:
     string longestPalindrome(string s) {
-        if(s.size()==1) return s;
-        vector<vector<int>> map (26, vector<int> ());
-        string ans;
-        for(int i=0;i<s.size();i++) map[int(s[i])-int('a')].push_back(i);
-        for (int l = 0; l < s.size(); l++) {
-            for (int r = 0; r < map[int(s[l]) - int('a')].size(); r++) {
-                int rightIndex = map[int(s[l]) - int('a')][r];
-                if (rightIndex > l) { 
-                    if (isLegalPalindrome(l + 1, rightIndex - 1, s)) {
-                        int length = rightIndex - l + 1;
-                        if (ans.size() < length) {
-                            ans = s.substr(l, length);
-                        }
+        vector<vector<bool>> dp (s.size(), vector<bool>(s.size()));
+        int begin = 0, maxLen = 1;
+        for(int i=0;i<s.size();i++) dp[i][i] = true;
+
+        for(int L=1;L<=s.size();L++) {
+            for(int i=0;i<s.size();i++) {
+                int j=i+L;
+                if(j>=s.size()) break;
+                if(s[i]!=s[j]) {
+                    dp[i][j] = false;
+                } else {
+                    dp[i][j] = j-i<3 ? true : dp[i+1][j-1];
+                }
+                if(dp[i][j]) {
+                    if(L+1>maxLen) {
+                        begin = i, maxLen = j-i+1;
                     }
                 }
             }
         }
-        return ans;
-    }
-
-    bool isLegalPalindrome(int l, int r, string s) {
-        while(l<r) {
-            if(s[l]!=s[r]) return false;
-            l++, r--;
-        }
-        return true;
+        
+        return s.substr(begin, maxLen);
     }
 };

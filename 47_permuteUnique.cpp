707932dc5cef9949c28cefdf1036cc2f5e083c2a
04megumi <wzy12241129@qq.com>
2025-05-04 12:52:38 +0800

    需要剪枝优化

---
+++ 47_permuteUnique.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> nums2;
+        unordered_map<int,bool> hashmap;
+        for(auto& num : nums) {
+            if(!hashmap[num]) {
+                hashmap[num] = true;
+                nums2.push_back(num);
+            }
+        }
+        return permute(nums2);
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        vector<bool> used(nums.size());
+        dfs(nums, 0, ans, temp, used);
+        return ans;
+    }
+    void dfs(vector<int>& nums, unsigned char index, vector<vector<int>>& ans, vector<int>& temp, vector<bool>& used) {
+        if(index==nums.size()) {
+            ans.push_back(temp);
+            return;
+        }
+        for(int i=0;i<nums.size();i++) {
+            if(used[i]) continue;
+            temp.push_back(nums[i]);
+            used[i] = true;
+            dfs(nums, index+1, ans, temp, used);
+            used[i] = false;
+            temp.pop_back();
+        }
+    }
+};

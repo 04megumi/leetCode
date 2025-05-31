79f7b05e082e03008435e27fefef04768d12986d
04megumi <wzy12241129@qq.com>
2025-06-01 00:19:25 +0800

    20250531

---
+++ 117_connect.cpp
@@ -0,0 +1,38 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        queue<Node*> q;
+        q.push(root);
+        if(!root) return nullptr;
+        while(!q.empty()) {
+            Node* pre = nullptr;
+            int size = q.size();
+            for(int i=0;i<size;i++) {
+                if(pre) pre->next = q.front();
+                pre = q.front();
+                if(q.front()->left) q.push(q.front()->left);
+                if(q.front()->right) q.push(q.front()->right);
+                q.pop();
+            }
+        }
+        return root;
+    }
+};

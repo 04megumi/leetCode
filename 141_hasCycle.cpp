b8231707f18d6b20ac083ec2f04520d2ff99d617
04megumi <wzy12241129@qq.com>
2025-06-16 16:21:16 +0800

    hasCycle

---
+++ 141_hasCycle.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        // fast 不为nullptr的时候 slow必然不为nullptr 所以fast作为条件
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+};

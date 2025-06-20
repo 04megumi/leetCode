5129b0102db8e2a879cc119c65ebf257d3a669ed
04megumi <wzy12241129@qq.com>
2025-06-20 22:36:12 +0800

    修改写法

--- 153_findMin.cpp
+++ 153_findMin.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        int l = 0, r = nums.size()-1, n = nums.size();
-        while(l <= r) {
+        int l = 0, r = nums.size()-1;
+        while(l < r) {
             int m = (l+r) >> 1;
-            if(nums[m]>nums[(m+1)%n]) return nums[(m+1)%n];
-            if(nums[l]<=nums[m]) {
-                l = m+1;
+            if(nums[l] > nums[r]) {
+                if(nums[m] >= nums[l]) {
+                    l = m+1;
+                } else {
+                    r = m;
+                }
             } else {
-                r = m-1;
+                return nums[l];
             }
         }
-        return nums[0];
+        return nums[l];
     }
 };

bce02d8efb4b3f9851229b444e5c5ef8e0c29502
04megumi <wzy12241129@qq.com>
2025-07-17 16:55:28 +0800

    feat: kmp算法

--- 28_strStr.cpp
+++ 28_strStr.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if(needle.size() > haystack.size()) return -1;
+        vector<int> prefix(needle.size());
+        getPrefix(prefix,needle);
+        int i = 0, j = 0;
 
-        for(int i=0;i+needle.size()<=haystack.size();i++) {
-            bool flag = true;
-            for (int j=0;j<needle.size();j++) {
-                if(needle[j]!=haystack[i+j]) {
-                    flag = false;
+        while(i<haystack.size() && j<needle.size()) {
+            if(haystack[i]==needle[j]) {
+                i++, j++;
+            } else {
+                if(!j) {
+                    i++;
+                } else {
+                    j = prefix[j-1]+1;
+                }
+            }
+        }
+
+        return j==needle.size() ? i-j : -1;
+    }
+    void getPrefix(vector<int>& prefix, string needle) {
+        prefix[0] = -1;
+        for(int i=1;i<needle.size();i++) {
+            int j = prefix[i-1];
+            while(j != -1) {
+                if(needle[j+1]!=needle[i]) {
+                    j = prefix[j];
+                } else {
                     break;
                 }
             }
-            if(flag) return i;
+            prefix[i] = needle[j+1]==needle[i]? j+1 : j;
         }
-
-        return -1;
     }
 };
